5677cb1d8e17b3102889e5b5a67b24eb238575c9
SAMthomas7 <samthomas.k7@gmail.com>
2024-11-12 15:52:14 +0530

    pages

--- SpeakEsy/src/App.jsx
+++ SpeakEsy/src/App.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login'; // Adjust the import path as necessary
 import HomePage from './pages/home';
 import UploadPage from './pages/upload';
-import TranscriptPage from './pages/transcript';
-import SummaryPage from './pages/summary';
+import ResultPage from './pages/result';
 
 function App() {
   return (
@@ -13,8 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} /> {/* Login Page at root */}
         <Route path="/home" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage/>}/>
-        <Route path="/transcript" element={<TranscriptPage/>}/>
-        <Route path="/summary" element={<SummaryPage/>}/>
+        <Route path="/result" element={<ResultPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </Router>
