9f243a58849ab5a85faeb8797b1e0fd236fb4825
Jaunty Paul <jonathanpaduthala@gmail.com>
2025-02-02 23:50:08 +0530

    Connection with Firebase is done in the frontend

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRGzmRt51CZf690KR_aOvoQHROfGPYcsg",
+    authDomain: "speakeazy-eee78.firebaseapp.com",
+    projectId: "speakeazy-eee78",
+    storageBucket: "speakeazy-eee78.firebasestorage.app",
+    messagingSenderId: "1003136291382",
+    appId: "1:1003136291382:web:53f0039b0f510c09257e97",
+    measurementId: "G-KRK3G8QW36"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup };
